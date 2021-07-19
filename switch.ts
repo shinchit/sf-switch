8ff5744ced4b0402ef8db09a8b6e9029c682ed1c
shinchit <shinchi.takahiro@classmethod.jp>
2021-07-19 10:22:22 +0900

    Implemented the function to turn on / off all processes of Process Builder.

--- switch.ts
+++ switch.ts
@@ -1,22 +1,47 @@
-import jsforce, { Connection, StandardSchema } from 'jsforce';
+import * as fs from "fs";
+import * as yargs from "yargs";
+import * as jsforce from "jsforce";
 import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname+'/.env' });
 
+// accept command line arguments
+let { argv } = <any>{};
+argv = yargs
+  .option("mode", {
+    description: "required. You have to specify running mode 'on' or 'off'",
+    demandOption: false,
+    default: "off"
+  })
+  .help().argv;
+
+const DEFINITION_DATA_FILE_PATH = __dirname + '/data/' + process.env.SF_USERNAME + '_define.json';
+
 (async () => {
-  // Specify schema of connecting organization
-  const conn = new Connection<StandardSchema>({
+  const conn = new jsforce.Connection({
     loginUrl : 'https://' + process.env.DOMAIN + '.salesforce.com'
   });
   await conn.login(process.env.SF_USERNAME, process.env.SF_PASSWORD);
 
-  // fetch records using find()
-  const recs = await conn.sobject('Opportunity').find({
-    CloseDate: { $lte: jsforce.Date.YESTERDAY },
-    IsClosed: true
-  });
-
-  // output retrieved records info
-  for (const rec of recs) {
-    console.log(rec.Amount, rec.Name, rec.LastActivityDate);
+  // fetch FlowDefinition using Tooling API
+  const flowDefinitions = await conn.tooling.query('SELECT Id, ActiveVersion.VersionNumber, LatestVersion.VersionNumber, DeveloperName FROM FlowDefinition');
+  let records: any[] = flowDefinitions['records'];
+  if ( argv.mode === 'on' ) {
+    if ( fs.existsSync(DEFINITION_DATA_FILE_PATH) ) {
+      records = JSON.parse(fs.readFileSync(DEFINITION_DATA_FILE_PATH, 'utf8'));
+    }
+  } else {
+    if ( ! fs.existsSync(DEFINITION_DATA_FILE_PATH) ) {
+      fs.writeFileSync(DEFINITION_DATA_FILE_PATH, JSON.stringify(flowDefinitions['records'], null, 2));
+    }
   }
+
+  records.forEach(function(flow: any) {
+    const activeVersionNumber = argv.mode === 'on' ? flow['ActiveVersion']['VersionNumber'] : null;
+    const res = conn.tooling.sobject('FlowDefinition').update({
+      Id: flow['Id'],
+      Metadata: {
+        'activeVersionNumber': activeVersionNumber
+      }
+    });
+  });
 })();
